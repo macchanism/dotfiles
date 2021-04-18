2e1698a068090c9a8374f847014a5b4bdd93a926
matchanism <matchatheism@gmail.com>
2021-04-18 14:12:53 +0900

    cp and generate .tcshrc and install_tcsh.sh

---
+++ install_tcsh.sh
@@ -0,0 +1,22 @@
+set -u
+
+# $THIS_DIR := install.shのあるディレクトリのpath
+THIS_DIR=$(cd $(dirname $0); pwd)
+
+# $BASEDIR := dotfiles
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+
+List0=(".bashrc.local")
+
+for item in ${List0[@]}; do
+  touch $HOME/$item
+done
+
+List1=(".common_shrc" ".bashrc")
+
+for item in ${List1[@]}; do
+  ln -snfv ${PWD}/$item $HOME/
+done
+
+source $HOME/.bashrc
