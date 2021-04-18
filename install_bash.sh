2d5b83eb41a3cf8d64f76569f1cac686c2ff73b3
matchanism <matchatheism@gmail.com>
2021-04-18 12:50:15 +0900

    cp .zshrc as .bashrc and install_zsh.sh as install_bash.sh

---
+++ install_bash.sh
@@ -0,0 +1,20 @@
+set -u
+
+# $THIS_DIR := install.shのあるディレクトリのpath
+THIS_DIR=$(cd $(dirname $0); pwd)
+
+# $BASEDIR := dotfiles
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+
+List0=(".zshrc.local")
+
+for item in ${List0[@]}; do
+  touch $HOME/$item
+done
+
+List1=(".common_shrc" ".zshrc")
+
+for item in ${List1[@]}; do
+  ln -snfv ${PWD}/$item ~/
+done
