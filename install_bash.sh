ea96f5efe11454efdbafc673c6226fe84b39d099
matchanism <matchatheism@gmail.com>
2021-04-18 12:51:45 +0900

    rewrite install_bash.sh

--- install_bash.sh
+++ install_bash.sh
@@ -7,13 +7,13 @@ THIS_DIR=$(cd $(dirname $0); pwd)
 BASEDIR=$(dirname $0)
 cd $BASEDIR
 
-List0=(".zshrc.local")
+List0=(".bashrc.local")
 
 for item in ${List0[@]}; do
   touch $HOME/$item
 done
 
-List1=(".common_shrc" ".zshrc")
+List1=(".common_shrc" ".bashrc")
 
 for item in ${List1[@]}; do
   ln -snfv ${PWD}/$item ~/
